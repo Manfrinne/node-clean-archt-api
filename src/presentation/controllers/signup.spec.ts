cd9797097d4b532f9978922c0d9a2d678ad08a49
manfrinne <m4nfrinne@gmail.com>
2023-04-06 12:33:00 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,21 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => { 
+    // Classe sut (System Under Test).
+    const sut = new SignUpController()
+
+    // Objeto que a classe precisa receber.
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    // A função 'handle' da classe sut precisa retornar status 400.
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+   })
+})
