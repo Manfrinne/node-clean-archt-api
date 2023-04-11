a6f42ffd491d13e848686d4efce6944c389308f4
manfrinne <m4nfrinne@gmail.com>
2023-04-11 01:13:44 -0300

    test: ensure SignUpController returns 400 if no password confirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -24,10 +24,8 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if no email is provided', () => { 
-    // Classe sut (System Under Test).
     const sut = new SignUpController()
 
-    // Objeto que a classe precisa receber.
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -36,17 +34,14 @@ describe('SignUp Controller', () => {
       }
     }
 
-    // A função 'handle' da classe sut precisa retornar status 400.
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  test('Should return 400 if no password is provided', () => { 
-    // Classe sut (System Under Test).
+  test('Should return 400 if no password is provided', () => {
     const sut = new SignUpController()
 
-    // Objeto que a classe precisa receber.
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -55,9 +50,24 @@ describe('SignUp Controller', () => {
       }
     }
 
-    // A função 'handle' da classe sut precisa retornar status 400.
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('Should return 400 if no password confirmation is provided', () => { 
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 })
