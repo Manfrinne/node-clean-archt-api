fa5426393b9d7731118a3573b4fa85ae42e12b7a
manfrinne <m4nfrinne@gmail.com>
2023-04-11 12:09:30 -0300

    test: ensure SignUpController returns 400 if an invalid email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,16 +1,35 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
+import { EmailValidator } from '../protocols/email-validator'
 
-// Criar um factory para não precisar repetir código quando
-// inserimos as dependências.
-const makeSut = (): SignUpController => {
-  return new SignUpController()
+// Interface para conseguir manipular e utilizar o Mock SutStub tanto na injeção de dependência como também na verificação de parâmetros obrigatórios.
+interface SutType {
+  sut: SignUpController
+  emailValidatorStub: EmailValidator
+}
+
+// Com o Factory não precisaremos repetir código em cada teste quando injetar dependências.
+const makeSut = (): SutType => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  // Stub é um tipo de Mock
+  const emailValidatorStub  = new EmailValidatorStub()
+  const sut = new SignUpController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => { 
     // Classe sut (System Under Test).
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     // Objeto que a classe precisa receber.
     const httpRequest = {
@@ -30,7 +49,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if no email is provided', () => { 
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -46,7 +65,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if no password is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -62,7 +81,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if no password confirmation is provided', () => { 
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
@@ -75,5 +94,26 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+
+  test('Should return 400 if an invalid email is provided', () => {
+    // Criamos a interface sutType, para poder acessar o emailValidatorStub 
+    const { sut, emailValidatorStub } = makeSut()
+    
+    // Agora com o Jest, conseguimos modificar o MockStub que padronizamos
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 })
