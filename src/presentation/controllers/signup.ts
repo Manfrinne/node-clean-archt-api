9151f0f4853c10ca868edeb2d436c62648a0a6c4
manfrinne <m4nfrinne@gmail.com>
2023-04-11 19:05:46 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,6 +4,7 @@ import { badRequest } from "../helpers/http-helper"
 import { Controller } from "../protocols/controller"
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from "../errors/invalid-param-error"
+import { ServerError } from "../errors/server-error"
 
 export class SignUpController implements Controller {
   // O private readonly significa uma classe c/ escopo local
@@ -15,18 +16,23 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    // Verificação dos parâmetros obrigatório
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-
-    // Verificação da injeção de dependência. Importante aqui é que conseguimos verificar a dependência mesmo antes de cria-la, seguindo apensa a noção de contrato determinado no protocols. Aplicação de Inversão de Dependência.
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
+  
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
